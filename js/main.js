64dc4a352ac5f5d8a84f6d9b398aeb8a5709b847
James M <james.e.n.morris@gmail.com>
2014-09-06 17:12:05 +0100

    Update main.js
    test

--- js/main.js
+++ js/main.js
@@ -9,18 +9,15 @@ $(function (){
 			links();
 			twitter();
 			websiteLink();
-			websiteLinkVisited();
 		}
 	});
 
 	// work out portfolio link
 	function websiteLink() {
 		$('table td a:not([href*="twitter"])').parent().addClass('website-link');
-	}
-	
-	function websiteLinkVisited() {
 		$('table td.website-link a:visited').addClass('visited');
 	}
+	
 
 	// add boostrap style to table
 	$('table').addClass('table table-striped table-hover');
